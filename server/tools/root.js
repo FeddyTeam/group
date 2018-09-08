9d0ca6966a1447334d1d0364dc123179b6eded57
bin16 <bin16@outlook.com>
2018-09-08 21:06:36 +0800

    fx

--- server/tools/root.js
+++ server/tools/root.js
@@ -1,27 +1,29 @@
+require('dotenv').config()
+
 const bcrypt = require('bcrypt')
-const config = require('../knex/knexfile.js')['production']
+const environment = process.env.NODE_ENV
+const config = require('../knex/knexfile.js')[environment]
 const knex = require('knex')(config)
-const cfg = require('dotenv').config().parsed
 
-const {
-    ROOT_EMAIL,
-    ROOT_USERNAME,
-    ROOT_PASSWORD
-} = cfg
+console.log(config, environment)
 
-console.log(ROOT_EMAIL, ROOT_PASSWORD, ROOT_USERNAME)
+// const {
+//     ROOT_EMAIL,
+//     ROOT_USERNAME,
+//     ROOT_PASSWORD
+// } = process.env
 
-knex('users').del().insert([{
-    id: 'root',
-    username: ROOT_USERNAME,
-    email: ROOT_EMAIL,
-    password: bcrypt.hashSync(ROOT_PASSWORD, 6),
-    name: 'Root',
-    avatar: 'https://static.feddy.org/FtnqM8NupRtfw_2-BUArl_nl_GiV?imageView/1/w/256',
-    birthday: '2018-09-09',
-    createdAt: new Date(),
-    updatedAt: new Date(),
-    adm: true,
-    cms: true,
-    abc: true
-}]).then(console.info)
+// knex('users').del().insert([{
+//     id: 'root',
+//     username: ROOT_USERNAME,
+//     email: ROOT_EMAIL,
+//     password: bcrypt.hashSync(ROOT_PASSWORD, 6),
+//     name: 'Root',
+//     avatar: 'https://static.feddy.org/FtnqM8NupRtfw_2-BUArl_nl_GiV?imageView/1/w/256',
+//     birthday: '2018-09-09',
+//     createdAt: new Date(),
+//     updatedAt: new Date(),
+//     adm: true,
+//     cms: true,
+//     abc: true
+// }]).then(console.info)
